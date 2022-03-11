04549692271ec647df2d3ea37db9f1798daa682d
Joseph Endale <jose.endal45@gmail.com>
2022-03-11 02:30:41 +0000

    Wrote a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
 **/
 int main(void)
 {
-	writef(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	printf(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 
 	return (1);
 }
