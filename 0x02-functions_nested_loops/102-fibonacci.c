b6c7c9028fe646a2ad23e0b3f005742b4cc49653
Joseph Endale <jose.endal45@gmail.com>
2022-03-16 22:00:02 +0000

    A program that prints the first 50 Fibonacci numbers, starting with 1 and 2, followed by a new line.

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,6 +10,7 @@ int main(void)
 long int i, j, k, next;
 j = 1;
 k = 2;
+{
 for (i = 1; i <= 50; ++i)
 {
 if (j != 20365011074)
@@ -22,6 +23,7 @@ printf("%ld\n", j);
 next = j + k;
 j = k;
 k = next;
+}
 }
 return (0);
 }
