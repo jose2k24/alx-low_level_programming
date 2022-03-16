a9d9cfb37764bc51614dac3e708e9bf1a5cb321a
Joseph Endale <jose.endal45@gmail.com>
2022-03-16 21:43:39 +0000

    A program that computes and prints the sum of all the multiples of 3 or 5 below 1024 (excluded), followed by a new line

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -17,7 +17,9 @@ for (i = 0; i < 1024; ++i)
 if ((i % 3) == 0)
 {
 sum3 = sum3 + i;
-} else if ((i % 5) == 0)
+}
+{
+else if ((i % 5) == 0)
 {
 sum5 = sum5 + i;
 }
@@ -25,4 +27,5 @@ sum5 = sum5 + i;
 sum = sum3 + sum5;
 printf("%lu\n", sum);
 return (0);
+}
 }
