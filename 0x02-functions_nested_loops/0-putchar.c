3e9cfaee8ea58fcf3f4869f30ee8c1343c3c6d70
Joseph Endale <jose.endal45@gmail.com>
2022-03-16 19:54:24 +0000

    A program that prints _putchar, followed by a new line.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *  * main - main block
+ *   * Return: Always 0 (Success)
+**/
+int main(void)
+{
+{
+printf("_putchar");
+}
+return (0);
+}
