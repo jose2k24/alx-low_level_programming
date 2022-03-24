45572ae68db6f89b20d9e7b4ef0e2d5cd7ba6e4a
Joseph Endale <jose.endal45@gmail.com>
2022-03-24 14:44:34 +0000

    A function that encodes a string using rot13.

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -13,7 +13,7 @@ for (i = 0; s[i] != '\0'; i++)
 {
 for (ii = 0; ii < 54; ii++)
 {
-if (((s[i] <= 'z' && s[i] >= 'a') || (s[i] <= 'Z' && s[i] >= 'A')) && s[i] == input[ii])
+if (((s[i] <= 'z' && s[i] >= 'a') || (s[i] <= 'Z' && s[i] >= 'A'))&& s[i] == input[ii])
 {
 s[i] = output[ii];
 break;
