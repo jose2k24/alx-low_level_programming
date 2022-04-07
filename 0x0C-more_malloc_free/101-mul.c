c6c4d9c0991993e0d8c415803f0c6a4de47fb154
Joseph Endale <jose.endal45@gmail.com>
2022-04-07 17:47:28 +0000

    A program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -14,7 +14,7 @@ while (s[i])
 if (s[i] < '0' || s[i] > '9')
 return (0);
 i++;
-}i
+}
 return (1);
 }
 /**
@@ -73,6 +73,7 @@ carry /= 10;
 }
 if (carry > 0)
 result[len1 + len2 + 1] += carry;
+}
 for (i = 0; i < len - 1; i++)
 {
 if (result[i])
